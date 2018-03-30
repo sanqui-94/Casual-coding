e83d8419a8f4a1563f7a6638b801285c5de65c73
Sanqui <sanqui.94@gmail.com>
2018-03-30 19:11:35 -0400

    this script deletes duplicates on a list, it converts it to a set for it

---
+++ no_duplicates_with_sets.py
@@ -0,0 +1,19 @@
+def delete_duplicates(a_list):
+    """this function will recive a list as parameter, then it will remove
+    every duplicate inside that list, we will be using sets on this one"""
+    a_set = set()
+    for i in a_list:
+    	a_set.add(i)
+    return a_set
+
+
+def main():
+	print "Please, introduce a list of things you want:"
+	raw_list = raw_input("> ")
+	a_list = raw_list.split()
+	print "your new list would be this:"
+	print delete_duplicates(a_list)
+
+
+if __name__ == '__main__':
+	main()
